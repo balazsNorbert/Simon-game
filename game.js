500787de1b65a3753cc97706206f1a016d9c4b60
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-01-21 21:25:58 +0200

    Sound effect by click

--- game.js
+++ game.js
@@ -1,14 +1,23 @@
+var userClickedPattern = [];
 var gamePattern=[];
 var buttonColours=["red","blue","green","yellow"];
 
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour=buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+  playSound(randomChosenColour);
 }
 nextSequence();
